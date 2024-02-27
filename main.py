d632ac6b8be58840696d8255ce0a099588776fe8
Noobpro112 <reismatheusyan83@gmail.com>
2024-02-27 19:04:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.clean()
+    page.add(ft.AppBar(title=ft.Text('Login!'), center_title=True,))
+
     def on_click(event):  
         name_insert = username.value
         password_insert = password.value
-        if name_insert == 'ADMIN' and password_insert == 'ADMIN':
+        if name_insert == '' and password_insert == '':
             segunda_pagina(page)
             page.update()
         else:
@@ -17,9 +20,40 @@ def main(page: ft.Page):
     page.add(password)
     page.add(ft.FloatingActionButton(icon=ft.icons.PASSWORD_OUTLINED, on_click=on_click))
 
-def segunda_pagina(page):
-    # Segunda página
-    page.clean()
-    page.add(ft.Text('Esta é a segunda página'))
 
+def segunda_pagina(page):
+    page.clean()
+    page.add(
+        ft.AppBar(
+            leading=ft.FilledTonalButton("Login", icon='add_shopping_cart', on_click=lambda event: main(page)),
+            leading_width=150,
+            title=ft.Text('Mercado Online!'),
+            center_title=True,
+        ),
+        ft.Column(
+            [
+                ft.Container(
+                    bgcolor=ft.colors.AMBER,
+                    alignment=ft.alignment.center,
+                    expand=True,
+                ),
+                ft.Divider(),
+                ft.Container(bgcolor=ft.colors.PINK, alignment=ft.alignment.center, expand=True),
+                ft.Divider(height=1, color="white"),
+                ft.Container(
+                    bgcolor=ft.colors.BLUE_300,
+                    alignment=ft.alignment.center,
+                    expand=True,
+                ),
+                ft.Divider(height=9, thickness=3),
+                ft.Container(
+                    bgcolor=ft.colors.DEEP_PURPLE_200,
+                    alignment=ft.alignment.center,
+                    expand=True,
+                ),
+            ],
+            spacing=0,
+            expand=True,
+        ),
+    )
 ft.app(target=main)

7651dfcf07ffd97506cf54e425629dba3f139482
Matheus Yan <reismatheusyan83@gmail.com>
2024-02-28 09:08:44 -0300

    Register page

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import flet as ft
 
+
 def main(page: ft.Page):
     page.clean()
     page.add(ft.AppBar(title=ft.Text('Login!'), center_title=True,))
 
-    def on_click(event):  
+    def on_click(event):
         name_insert = username.value
         password_insert = password.value
         if name_insert == '' and password_insert == '':
+            #sql inner join
             segunda_pagina(page)
             page.update()
         else:
@@ -18,50 +20,105 @@ def main(page: ft.Page):
 
     page.add(username)
     page.add(password)
-    page.add(ft.FloatingActionButton(icon=ft.icons.PASSWORD_OUTLINED, on_click=on_click))
+    page.add(ft.FloatingActionButton(
+        icon=ft.icons.PASSWORD_OUTLINED, on_click=on_click))
+    page.add(ft.FilledButton(text='Registre-se',on_click=lambda event:registro(page)))
 
 
+import flet as ft
 def segunda_pagina(page):
     page.clean()
     page.add(
         ft.AppBar(
-            leading=ft.FilledTonalButton("Login", icon='add_shopping_cart', on_click=lambda event: main(page)),
+            leading=ft.FilledTonalButton(
+                "Login", icon='add_shopping_cart', on_click=lambda event: main(page)),
             leading_width=150,
             title=ft.Text('Mercado Online!'),
             center_title=True,
         ),
         ft.Column(
             [
-                ft.Container(
-                    bgcolor=ft.colors.AMBER,
-                    alignment=ft.alignment.center,
-                    expand=False,
-                    height=100,
-                    padding=2
-                    
+                ft.Row(
+                    [
+                        ft.Container(
+                            bgcolor=ft.colors.AMBER,
+                            alignment=ft.alignment.center,
+                            expand=False,
+                            height=100,
+                            padding=2,
+                            width=300,
+                            ink=True,
+                            on_click=lambda e:print('Click')
+                        ),
+                        ft.Divider(height=3, color="white"),
+                        ft.Container(
+                            bgcolor=ft.colors.AMBER,
+                            alignment=ft.alignment.center,
+                            expand=False,
+                            height=100,
+                            padding=2,
+                            width=300,
+                            ink=True,
+                            on_click=lambda e:print('Click')
+                        ),
+                        ft.Divider(height=3, color="white"),
+                        ft.Container(
+                            bgcolor=ft.colors.AMBER,
+                            alignment=ft.alignment.center,
+                            expand=False,
+                            height=100,
+                            padding=2,
+                            width=300,
+                            ink=True,
+                            on_click=lambda e:print('Click')
+                        ),
+                        ft.Divider(height=3, color="white"),
+                        ft.Container(
+                            bgcolor=ft.colors.AMBER,
+                            alignment=ft.alignment.center,
+                            expand=False,
+                            height=100,
+                            padding=2,
+                            width=300,
+                            ink=True,
+                            on_click=lambda e:print('Click')
+                        ),
+                        ft.Divider(height=3, color="white"),
+                    ],
+                    alignment=ft.alignment.center,  # Centraliza a linha na página
                 ),
-                ft.Divider(),
-                ft.Container(bgcolor=ft.colors.PINK, alignment=ft.alignment.center, expand=False, height=100,padding=2),
-                ft.Divider(height=3, color="white"),
-                ft.Container(
-                    bgcolor=ft.colors.BLUE_300,
-                    alignment=ft.alignment.center,
-                    expand=False,
-                    height=100,
-                    padding=2,
-                    
-                ),
-                ft.Divider(height=9, thickness=3),
-                ft.Container(
-                    bgcolor=ft.colors.DEEP_PURPLE_200,
-                    alignment=ft.alignment.center,
-                    expand=False,
-                    height=100,
-                    padding=2
-                ),
+                # Adicione mais linhas aqui para cada linha de produtos
             ],
             spacing=4,
             expand=False,
         ),
     )
+
+def registro(page):
+    page.clean()
+    page.add(ft.AppBar(title=ft.Text('Registro!'), center_title=True,))
+
+    def on_click(event):
+        name_insert = username.value
+        password_insert = password.value
+        email_insert = email.value
+        if name_insert == '' and password_insert == '' and email_insert=='':
+            # SQL Insert
+            segunda_pagina(page)
+            page.update()
+        else:
+            page.add(ft.Text(f'Algo deu errado!'))
+            page.update()
+
+    username = ft.TextField(hint_text="Username: ")
+    password = ft.TextField(hint_text="Password: ")
+    email = ft.TextField(hint_text='Digite o seu email: ')
+
+    page.add(username)
+    page.add(password)
+    page.add(email)
+    page.add(ft.FloatingActionButton(
+        icon=ft.icons.PASSWORD_OUTLINED, on_click=on_click))
+
+
 ft.app(target=main)
